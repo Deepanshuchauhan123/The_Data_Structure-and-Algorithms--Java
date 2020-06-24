7ae320cd3f0355f961847cac4db843ba4540ea36
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2020-06-24 09:52:23 +0530

    BST Traversal

---
+++ Data Structure/Binary Search Tree(BST)/Dynamic_Binary_Search_Tree_Inorder_Traversal.java
@@ -0,0 +1,108 @@
+/*
+Binary Search Tree:
+		All smaller node than parent node should go to the 
+		left and larger will go to the right of parent.
+*/
+import java.util.Scanner;
+
+public class Dynamic_Binary_Search_Tree_Inorder_Traversal{
+    static Node root=null;
+
+    // Main function for creating node.
+    public static void main(String[] args)
+    {
+        System.out.println(":::::::::::: Binary Search Tree ::::::::");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true)
+        {
+            System.out.println("\n1. Add Node to the Binary Search Tree:");
+            System.out.println("2. Inorder Traversal of Binary Search Tree:");
+            System.out.println("3. Exit");
+            System.out.println("Enter your Choice:");
+            int choice = sc.nextInt();
+            switch (choice)
+            {
+                case 1:
+                    System.out.print("Enter node Data: ");
+                    InsertNode(sc.nextInt());
+                    break;
+                case 2:
+	    System.out.print("In-Order Traversal is :: ");
+                    Inorder(root);
+                    break;
+                case 3:
+                    System.exit(1);
+                    break;
+                default:
+                    System.out.println("Enter valid choice!!!!");
+            }
+
+        }
+    }
+
+    public static void InsertNode(int value) {
+        Node temp = root, n = null;
+
+        // Firstly we should check that whether root is null or not, if null then  we will initialise the root node.
+        if (root == null) {
+            root=new Node(value);
+
+        } else {
+
+            // Then we will go to the leaf node where the correct position of the user inserted data.
+            while (temp != null)
+            {
+                if (value < temp.data)
+                {
+                    n = temp;
+                    temp = temp.LeftChild;
+                } else
+                    {
+                    n = temp;
+                    temp = temp.RightChild;
+                    }
+            }
+            // Then there we create a node and initialise that's nodes value.
+            Node node = new Node(value);
+            if (value < n.data)
+            {
+                n.LeftChild = node;
+            } else
+                {
+                n.RightChild = node;
+                }
+        }
+    }
+    public static void Inorder(Node temp)
+    {
+//if Node value becomes null that means their is no further nodes in the tree so it return.
+        if (temp == null)
+        {
+            return;
+        }
+        //Left node will be called
+        Inorder(temp.LeftChild);
+        //As on second time as node will be traversed it will print the value.
+        System.out.print(temp.data+" ");
+        //Right node will be called at last
+        Inorder(temp.RightChild);
+
+    }
+
+    //Their will be two parts of Node namely as LeftNode,RighNode as its a binary tree.
+    static class Node
+    {
+        int data;
+        Node LeftChild, RightChild;
+
+        //Constructor to initialise the value of the nodes of tree
+        Node(int Data) {
+            data = Data;
+            // As node created the left and right part of the node is by default null untill user doesn't add node.
+            LeftChild = null;
+            RightChild = null;
+        }
+    }//class node end here
+}//main program class end here
