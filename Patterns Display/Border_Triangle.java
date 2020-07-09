3cea56f57acff0bffcd84f8724e5cbba38984e7d
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2020-07-09 21:36:42 +0530

    Pattern

---
+++ Patterns Display/Border_Triangle.java
@@ -0,0 +1,35 @@
+/*
+Enter Rows: 
+5
+     *
+   *   *
+  *     *
+ *       *
+*********
+*/
+import java.util.Scanner;
+
+class Border_Triangle{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Rows: ");
+
+        int rows = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                if (k == 1 || i == rows || k == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+        sc.close();
+    }
+}
