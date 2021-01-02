f9042627f81995318fa9adc5af6e809e963f1efb
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2021-01-02 20:42:28 +0530

    Static

---
+++ HackerRank Practice/Static_Block.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Static_Block {
+static int B,H;
+static boolean flag=true;
+static{
+    Scanner sc = new Scanner(System.in);
+    B=sc.nextInt();
+    H=sc.nextInt();
+    if(B<=0 || H<=0){
+        flag=false;
+        System.out.print("java.lang.Exception: Breadth and height must be positive");
+    }
+}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
