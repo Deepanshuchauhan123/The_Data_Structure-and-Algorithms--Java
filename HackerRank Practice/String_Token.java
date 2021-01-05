64d0025f8fa2038b84f2a346430be55cfd67b239
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2021-01-05 15:49:40 +0530

    tokens

---
+++ HackerRank Practice/String_Token.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class String_Token {
+
+    public static void main(String[] args) 
+   {
+        Scanner scan = new Scanner(System.in);
+        
+        String s = scan.nextLine().trim();
+        
+        if(s.length()>0){
+
+        String[] tokens = s.split("[!,?._'@\\s]+");
+        System.out.println(tokens.length);
+        for(String token:tokens)
+            System.out.println(token);
+
+        }else{
+            System.out.println(0);
+        }
+        scan.close();
+    }
+}
